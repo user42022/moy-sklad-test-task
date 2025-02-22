24c544dc55408243619c9ec169e1cd16babf2753
user42022 <andrekizilov@yandex.ru>
2025-02-22 12:48:45 +0300

    добавил шрифты, переменные цветов и  брейкпоинтов и основные стили, добавил алиасы и обработку  составных классовых имен в  конфиг vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/assets/styles'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+    }
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    }
+  }
 })
